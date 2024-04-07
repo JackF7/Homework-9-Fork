06ea562e07f5d9ce4a465b8e914223861254826f
Jack Forester <jackforester@Jacks-MacBook-Pro-9.local>
2024-04-07 14:26:46 -0400

    Updated homework

--- app/models/Post.php
+++ app/models/Post.php
@@ -2,40 +2,10 @@
 
 namespace app\models;
 
+use app\core\Database;
+
 use PDO;
 use PDOException;
-trait Database
-{
-    private function connect()
-    {
-        $type = 'mysql';
-        $server = 'localhost';
-        $db = 'homework_9';
-        $port = '8888';
-        $charset = 'utf8mb4';
-
-        //the mamp default values here
-        $username = '';
-        $password = '';
-
-        $options = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            //set the default fetch type
-            //PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,/
-            PDO::ATTR_EMULATE_PREPARES => false,
-        ];
-
-        $dsn = "$type:host=$server;dbname=$db;port=$port;charset=$charset";
-
-        try {
-            return new PDO($dsn, $username, $password, $options);
-        } catch (PDOException $e) {
-            throw new PDOException($e->getMessage(), $e->getCode());
-        }
-    }
-}
-
 class Post
 {
     use Database;
